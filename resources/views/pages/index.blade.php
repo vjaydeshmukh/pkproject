1bef41acbd3e6165fbd6a584c6cea54638bef339
mathew borje <matborj21@gmail.com>
2020-03-21 16:57:56 +0800

    :tada: Initial commit and ADD PagesContoller, Set up its Route

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'PKProject') }}</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Welcome to Laravel Tuturioal for newb.</h1>
+        <p>This is a Laravel tutorial sereis</p>
+    </body>
+</html>
